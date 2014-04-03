77d13915ea0ad3698e9f6cfb641474b07f56e636
kenu <kenu.heo@gmail.com>
2014-04-04 02:26:07 +0900

    url pattern for static files

--- spring/mvc/src/main/java/com/okdevtest/spring/HomeController.java
+++ spring/mvc/src/main/java/com/okdevtest/spring/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
