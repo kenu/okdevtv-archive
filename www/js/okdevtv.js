f90bde714b08467e8bde8c5bc24f9b0fcea87e0f
kenu <kenu.heo@gmail.com>
2014-11-02 13:24:18 +0900

    tuned up

---
+++ www/js/okdevtv.js
@@ -0,0 +1,81 @@
+$(function () {
+    $("#form").on("submit", function () {
+        sendMessage();
+        return false;
+    });
+    getList();
+});
+
+function sendMessage() {
+    if ($("#name").val() && $("#message").val()) {
+        $.ajax({
+            url: "http://okdevtv.com:3000/register/",
+            data: {
+                category: 'kenu.heo@gmail.com',
+                name: $("#name").val(),
+                email: $("#email").val(),
+                message: $("#message").val()
+            },
+            dataType: "jsonp",
+            type: "POST"
+        })
+            .done(function (r) {
+                $("#result").html("sent:" + r);
+            });
+        // clear form
+        $("#form").find("input[type=text], textarea").val("");
+    } else {
+        if (!($("#name").val())) {
+            $("#name").focus();
+            return;
+        }
+        if (!($("#message").val())) {
+            $("#message").focus();
+            return;
+        }
+    }
+}
+function getList() {
+    $.ajax({url: "http://okdevtv.com:3000/list", dataType : "jsonp"})
+    .done(function(data){
+        var list = data.list;
+        for(var i in list) {
+            var row = '<div><span>' + list[i].message.linkify() + '</span>';
+            var datetime = $.datepicker.formatDate('yy/mm/dd', getDate(list[i]._id));
+            row += '<br><span>' + datetime + '</span>';
+            row += ' <span>' + list[i].name + '</span></div>';
+            $("#list").append(row);
+        }})
+    .fail(function(e){
+        console.log(e);
+    });
+}
+function getDate(_id) {
+    return new Date(parseInt(_id.substring(0, 8), 16) * 1000 );
+}
+
+/**
+ * "'버전의 의미 http://semver.org/lang/ko/ '".linkify()
+ * "'버전의 의미 <a href="http://semver.org/lang/ko/">http://semver.org/lang/ko/</a> '"
+ * http://stackoverflow.com/a/7123542
+ * @Roshambo
+ */
+if(!String.linkify) {
+    String.prototype.linkify = function() {
+
+        // http://, https://, ftp://
+        var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+
+        // www. sans http:// or https://
+        var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+
+        // Email addresses
+        var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+        return this
+            .replace(urlPattern, '<a href="$&">$&</a>')
+            .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+            .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+    };
+}
+
--- www/nodejs/server.js
+++ www/nodejs/server.js
@@ -1,4 +1,3 @@
-'use strict';
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
@@ -6,10 +5,10 @@ var MongoClient = require('mongodb').MongoClient;
 app.get('/register', function (req, res) {
     console.log(req.method);
     var msg = {
+        category: req.param("category"),
         name: req.param("name"),
         email: req.param("email"),
-        message: req.param("message"),
-        category: req.param("category")
+        message: req.param("message")
     };
 
     save(msg);
@@ -24,22 +23,7 @@ app.get('/register', function (req, res) {
 });
 
 app.get('/list', function (req, res) {
-    console.log(req.method);
-    var msg = {
-        name: req.param("name"),
-        email: req.param("email"),
-        message: req.param("message")
-    };
-
-    save(msg);
-
-    res.set('Content-Type', 'application/json');
-    var callback = req.raram("callback");
-    if (callback) {
-        res.send(callback + '({"success": true})');
-    } else {
-        res.send('{"success": true}');
-    }
+    list(req, res);
 });
 
 var server = app.listen(3000, function () {
@@ -49,7 +33,7 @@ var server = app.listen(3000, function () {
 function save(msg) {
     MongoClient.connect("mongodb://localhost/test",
         function (err, db) {
-            if (!err) {
+            if (err) {
                 console.log('err:' + err + '\ndb:' + db.collection("test"));
             }
             try {
@@ -61,4 +45,29 @@ function save(msg) {
                 console.log(e);
             }
         });
+}
+
+function list(req, res) {
+    var listData = [];
+    MongoClient.connect("mongodb://localhost/test",
+        function(err, db) {
+            if (err) {
+                console.log('err:' + err );
+                return;
+            }
+            db.collection("test").find().sort({_id: -1}).toArray(function (err, items) {
+            var callback = req.param("callback");
+            res.set('Content-Type', 'application/json');
+            for (var i in items) {
+                listData.push(items[i]);
+            }
+            if (callback) {
+                res.send(callback + '({"success": true, "list": ' + JSON.stringify(listData) + '})');
+            } else {
+                res.send('{"success": true, "list": ' + JSON.stringify(listData) + '}');
+            }
+            db.close();
+        });
+        }
+    );
 }
