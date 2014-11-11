bb1c55753fb2ba85bbcc23a1fe7219d4059ad3f0
kenu <kenu.heo@gmail.com>
2014-11-12 00:20:05 +0900

    1112

---
+++ www/nodejs/example.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function callback(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    
+    var path = url.parse(req.url).pathname;
+
+    fs.readFile(__dirname + path, 
+                function (err, content) {
+        res.end(content);
+    })
+}
+
+http.createServer(callback).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
