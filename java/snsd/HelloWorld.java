343d434010e0cf086182e8734aa1dc6819e6ccd7
kenu <kenu.heo@gmail.com>
2014-11-16 16:11:09 +0900

    java

---
+++ java/snsd/HelloWorld.java
@@ -0,0 +1,11 @@
+package snsd;
+
+public class HelloWorld {
+    public static void main(String... args) {
+        if (args.length > 0) {
+            System.out.println("Hello World! " + args[0]);
+        } else {
+            System.out.println("Hello World! snsd");
+        }
+    }
+}
