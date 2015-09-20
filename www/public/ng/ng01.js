a28bb8b00a56660e09879f51623bf80f678226a3
kenu <kenu.heo@gmail.com>
2015-09-20 12:23:10 +0900

    tomcat -> node-express

---
+++ www/public/ng/ng01.js
@@ -0,0 +1,11 @@
+(function(){
+    var app = angular.module('okdevtv', []);
+    var status = {
+        url: 'http://www.okdevtv.com/'
+    };
+
+    app.controller('OkController', function() {
+        this.gateway = status;
+    });
+})();
+
