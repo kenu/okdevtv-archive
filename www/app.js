6827777d1c69f78800e39b8047bb28d594ba6afb
kenu <kenu.heo@gmail.com>
2017-12-03 23:30:09 +0900

    refactor

--- www/app.js
+++ www/app.js
@@ -5,11 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var apis = require('./routes/apis');
-var users = require('./routes/users');
-var mib = require('./routes/mib');
-
 var app = express();
 
 // view engine setup
@@ -24,10 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/apis', apis);
-app.use('/users', users);
-app.use('/mib*', mib);
+app.use('/', require('./routes/index'));
+app.use('/apis', require('./routes/apis'));
+app.use('/users', require('./routes/users'));
+app.use('/mib*', require('./routes/mib'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
