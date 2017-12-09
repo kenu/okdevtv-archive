bed0b16fa91c0c81289f99863ee12691620d5151
kenu <kenu.heo@gmail.com>
2017-12-09 23:41:47 +0900

    rm java

--- java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String... args) {
-        if (args.length > 0) {
-            System.out.println("Hello World! " + args[0]);
-        } else {
-            System.out.println("Hello World! aoa");
-        }
-    }
-}
