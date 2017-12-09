0bfa4404bf9149d075cd2f80abd868f0a6597965
kenu <kenu.heo@gmail.com>
2017-12-09 23:41:17 +0900

    rm admin

--- admin/lib/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER || 'devuser',
-    password: process.env.DB_PASS || 'okpassokpass',
-    database: process.env.DB_NAME || 'okdevdb'
-});
-
-module.exports = connection;
