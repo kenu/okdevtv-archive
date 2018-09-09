73c7b8243b91fad2669ee487b8f53a6528a8b826
kenu.heo <kenu.heo@gmail.com>
2018-09-09 11:55:03 +0900

    memo2

---
+++ www/public/jsi/Chapter_07/ex7-memo2.js
@@ -0,0 +1,24 @@
+Function.prototype.memoization = function(key) {
+    var arg = Array.prototype.slice.call(arguments, 1);
+    this.data = this.data || {};
+
+    return this.data[key] !== undefined ?
+        this.data[key] : this.data[key] = this.apply(this, arg);
+};
+
+function myCalculate1(input) {
+    return input * input;
+}
+function myCalculate2(input) {
+    return input * input / 4;
+}
+
+myCalculate1.memoization(1, 5);
+myCalculate1.memoization(2, 4);
+myCalculate2.memoization(1, 6);
+myCalculate2.memoization(2, 7);
+
+console.log(myCalculate1.memoization(1));
+console.log(myCalculate1.memoization(2));
+console.log(myCalculate2.memoization(1));
+console.log(myCalculate2.memoization(2));
