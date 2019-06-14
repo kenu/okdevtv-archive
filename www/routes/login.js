1196e956e9298d5612685d3af9ceab940b71c76e
kenu <kenu@okjsp.pe.kr>
2019-06-14 21:24:31 +0900

    session issue

--- www/routes/login.js
+++ www/routes/login.js
@@ -1,27 +1,27 @@
-const express         =     require('express')
-  , passport          =     require('passport')
-  , FacebookStrategy  =     require('passport-facebook').Strategy
-  , session           =     require('express-session')
-  , cookieParser      =     require('cookie-parser')
-  , bodyParser        =     require('body-parser')
-  , config            =     require('../configuration/config')
-  , mysql             =     require('mysql');
+const express = require('express')
+  , passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , session = require('express-session')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , config = require('../configuration/config')
+  , mysql = require('mysql');
 const router = express.Router();
 
 //Define MySQL parameter in Config.js file.
 const pool = mysql.createPool({
-  host     : config.host,
-  user     : config.username,
-  password : config.password,
-  database : config.database
+  host: config.host,
+  user: config.username,
+  password: config.password,
+  database: config.database
 });
 
 // Passport session setup.
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
@@ -29,22 +29,22 @@ passport.deserializeUser(function(obj, done) {
 // Use the FacebookStrategy within Passport.
 
 passport.use(new FacebookStrategy({
-    clientID: config.facebook_api_key,
-    clientSecret:config.facebook_api_secret ,
-    callbackURL: config.callback_url
-  },
-  function(accessToken, refreshToken, profile, done) {
+  clientID: config.facebook_api_key,
+  clientSecret: config.facebook_api_secret,
+  callbackURL: config.callback_url
+},
+  function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
       //Check whether the User exists or not using profile.id
-      if(config.use_database) {
+      if (config.use_database) {
         // if sets to true
-        pool.query("SELECT * from user_info where user_id="+profile.id, (err,rows) => {
-          if(err) throw err;
-          if(rows && rows.length === 0) {
-              console.log("There is no such user, adding now");
-              pool.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+        pool.query("SELECT * from user_info where user_id=" + profile.id, (err, rows) => {
+          if (err) throw err;
+          if (rows && rows.length === 0) {
+            console.log("There is no such user, adding now");
+            pool.query("INSERT into user_info(user_id,user_name) VALUES('" + profile.id + "','" + profile.username + "')");
           } else {
-              console.log("User already exists in database");
+            console.log("User already exists in database");
           }
         });
       }
@@ -55,30 +55,33 @@ passport.use(new FacebookStrategy({
 
 router.use(cookieParser());
 router.use(bodyParser.urlencoded({ extended: false }));
-router.use(session({ secret: 'keyboard cat', key: 'sid'}));
+router.use(session({
+  secret: 'keyboard cat', key: 'sid', resave: true,
+  saveUninitialized: true
+}));
 router.use(passport.initialize());
 router.use(passport.session());
 router.use(express.static(__dirname + '/public'));
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
   console.log(req.session.passport);
   res.render('login', { user: req.user });
 });
 
-router.get('/account', ensureAuthenticated, function(req, res){
+router.get('/account', ensureAuthenticated, function (req, res) {
   res.render('account', { user: req.user });
 });
 
-router.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
 
 router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
-  function(req, res) {
+  passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
+  function (req, res) {
     res.redirect('/');
   });
 
-router.get('/logout', function(req, res){
+router.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
@@ -90,7 +93,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('login', { user: req.user });
 });
 
