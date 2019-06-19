cbd56ce6053cf21a3b89babf72d5b0963b71213c
kenu <kenu@okjsp.pe.kr>
2019-06-19 19:04:51 +0900

    set password

--- www/routes/user.js
+++ www/routes/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const user_service = require('../service/user_service');
 
-router.get('/signup', function(req, res) {
+router.get('/signup', function (req, res) {
   res.render('user/signup', {});
 });
 
-router.post('/signup', async function(req, res) {
+router.post('/signup', async function (req, res) {
   let email = req.body.email;
   let status = 'fail';
   let msg = '';
@@ -15,7 +15,7 @@ router.post('/signup', async function(req, res) {
     console.log(response[0].affectedRows);
     if (response[0].affectedRows === 1) {
       status = 'ok';
-    }  
+    }
   } catch (e) {
     msg = e.msg;
   }
@@ -29,19 +29,53 @@ router.post('/signup', async function(req, res) {
   res.json(result);
 });
 
-router.get('/setup', function(req, res) {
-  res.end('user password setup page\n');
+router.get('/setup', async function (req, res) {
+  const hash = req.query.q;
+  let msg = '';
+  try {
+    const result = await user_service.setUpAccount(hash);
+    console.log(result);
+  } catch (e) {
+    msg = e.msg;
+  }
+  if (msg) {
+    res.render('error', { message: msg });
+  } else {
+    res.render('user/set_password', { hash });
+  }
 });
 
-router.post('/setup', function(req, res) {
-  res.end('user setup check\n');
+router.post('/setup', async function (req, res) {
+  const password = req.body.password;
+  const password_confirm = req.body.password_confirm;
+  const hash = req.body.hash;
+  let status = 'fail';
+  let msg = '';
+
+  try {
+    const response = await user_service.setUpPassword(
+      { password, password_confirm, hash });
+    console.log(response[0].affectedRows);
+    if (response[0].affectedRows === 1) {
+      status = 'ok';
+    }
+  } catch (e) {
+    msg = e.msg;
+  }
+  let result = {
+    status: status
+  }
+  if (msg) {
+    result.msg = msg;
+  }
+  res.json(result);
 });
 
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
   res.end('user login page\n');
 });
 
-router.post('/login', function(req, res) {
+router.post('/login', function (req, res) {
   res.end('user login check\n');
 });
 
--- www/service/user_service.js
+++ www/service/user_service.js
@@ -1,6 +1,18 @@
 const knex = require('../lib/knex');
 const mail = require('../lib/mail');
 const uuidv4 = require('uuid/v4');
+const bcrypt = require('bcrypt');
+
+async function hashPassword(password) {
+    const saltRounds = 10;
+    const hashedPassword = await new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRounds, function (err, hash) {
+            if (err) reject(err);
+            resolve(hash);
+        });
+    });
+    return hashedPassword;
+}
 
 module.exports = {
     signupByEmail: async function (email) {
@@ -17,7 +29,7 @@ module.exports = {
         <p>아래 링크 페이지로 오셔서 가입을 완료하실 수 있습니다.</p>
         <p><a href="https://okdevtv.com/user/setup?q=${uuid}" target="_blank">
         https://okdevtv.com/user/setup?q=${uuid}</a></p>
-        <p></p><p>- Kenu @ OKdevTV`;
+        <p></p><p>- Kenu @ OKdevTV</p>`;
 
         // send email
         const send_result = await mail.send(email, message);
@@ -28,5 +40,40 @@ module.exports = {
             `insert into user_candidate 
 (seq, email, uuid, created_at) values (null, ?, ?, now());`, [email, uuid]);
         return save_result;
+    },
+    setUpAccount: async (hash) => {
+        const query = `select * from user_candidate
+where uuid = ? and finish = 'N';`;
+        const result = await knex.raw(query, [hash]);
+        if (result[0].length == 0) {
+            throw { msg: `invalid code` };
+        }
+        const { seq, email } = result[0][0];
+
+        const query_account = `insert into user (seq, email, created_at)
+values (null, ?, now());`;
+        await knex.raw(query_account, [email]);
+
+        const query_finish_candidate =
+            `update user_candidate set finish = 'Y', finished_at = now() where seq = ?;`;
+        const result_finish = await knex.raw(query_finish_candidate, [seq]);
+
+        return result_finish;
+    },
+    setUpPassword: async ({ password, password_confirm, hash }) => {
+
+        if (password.length < 8) {
+            throw '비밀번호는 8자리 이상으로 해주세요.';
+        }
+        if (password !== password_confirm) {
+            throw '입력하신 두 비밀번호가 다릅니다.';
+        }
+        const crypted_password = await hashPassword(password);
+        console.log(crypted_password);
+
+        const query = `update user set passwd = ?, updated_at = now()
+        where email = (select email from user_candidate where uuid = ?)`;
+        const result = await knex.raw(query, [crypted_password, hash]);
+        return result;
     }
 }
